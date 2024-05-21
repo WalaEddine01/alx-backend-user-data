b79109ef2a994fcfb12bacf3604f03c9b29ff0ae
walaeddine01 <walaaeddine33@gmail.com>
2024-05-21 23:29:10 +0200

    I made the changes on 2024-05-21 23:29:10

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """
+This is the auth Module
 """
 from flask import request, jsonify, abort
 from typing import List, TypeVar
@@ -7,18 +8,22 @@ from typing import List, TypeVar
 
 class Auth:
     """
+    This is the auth class
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
+        require_auth method
         """
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """
+        authorization_header method
         """
         return None
-    
-    def current_user(self, request=None) -> TypeVar('User'): # type: ignore
+
+    def current_user(self, request=None) -> TypeVar('User'):  # type: ignore
         """
+        current_user method
         """
         return None
