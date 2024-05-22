3874d483b77e6de1838c2519450bd7207d02b214
walaeddine01 <walaaeddine33@gmail.com>
2024-05-22 13:11:01 +0200

    I made the changes on 2024-05-22 13:11:01

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 This is the auth Module
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -12,9 +13,25 @@ class BasicAuth(Auth):
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
         """
-        This is the def extract_base64_authorization_header method
+        This is the extract_base64_authorization_header method
         """
         if not authorization_header or type(authorization_header) != str\
                 or not authorization_header.startswith("Basic "):
             return None
         return str(authorization_header.split(" ")[1])
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """
+        This is the decode_base64_authorization_header
+        """
+        if not base64_authorization_header\
+                or type(base64_authorization_header) != str:
+            return None
+        try:
+            return base64.b64decode(
+                base64_authorization_header
+                ).decode('utf-8')
+        except Exception:
+            return None
