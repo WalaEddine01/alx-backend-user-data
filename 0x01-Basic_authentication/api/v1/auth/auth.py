7bbe61cdc3155ef21ab698a1d3d6b5c8af93316b
walaeddine01 <walaaeddine33@gmail.com>
2024-05-22 11:56:31 +0200

    I made the changes on 2024-05-22 11:56:31

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -28,7 +28,9 @@ class Auth:
         """
         authorization_header method
         """
-        return None
+        if request is None or "Authorization" not in request.headers:
+            return None
+        return request.headers["Authorization"]
 
     def current_user(self, request=None) -> TypeVar('User'):  # type: ignore
         """
