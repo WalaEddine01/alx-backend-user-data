35fa3fd967ac6111144e303af7c0d21628b3af03
walaeddine01 <walaaeddine33@gmail.com>
2024-05-24 17:10:00 +0200

    I made the changes on 2024-05-24 17:10:00

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -45,42 +45,4 @@ class BasicAuth(Auth):
         """
         This is the method extract_user_credentials
         """
-        if not decoded_base64_authorization_header or\
-            type(decoded_base64_authorization_header) != str or\
-                len(decoded_base64_authorization_header.split(":")) == 1:
-            return None, None
-        user_data = decoded_base64_authorization_header.split(':')
-        return user_data[0], user_data[1]
-
-    def user_object_from_credentials(
-            self,
-            user_email: str,
-            user_pwd: str
-            ) -> TypeVar('User'):  # type: ignore
-        """ This is the user_object_from_credentials method
-        """
-        if not user_email or type(user_email) != str or\
-                not user_pwd or type(user_pwd) != str:
-            return None
-        try:
-            users = User.search({'email': user_email})
-        except Exception:
-            return None
-
-        for user in users:
-            if user.is_valid_password(user_pwd):
-                return user
-        return None
-
-    def current_user(self, request=None) -> TypeVar('User'):  # type: ignore
-        """
-        Tis is the current_user method
-        """
-        try:
-            auth_header = self.authorization_header(request)
-            encoded = self.extract_base64_authorization_header(auth_header)
-            decoded = self.decode_base64_authorization_header(encoded)
-            email, password = self.extract_user_credentials(decoded)
-            return self.user_object_from_credentials(email, password)
-        except Exception:
-            return None
+        pass
