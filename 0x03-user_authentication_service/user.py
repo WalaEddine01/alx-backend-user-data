79ed31e5df5639d0c105f0cd51796812254d185b
walaeddine01 <walaaeddine33@gmail.com>
2024-05-29 22:28:18 +0200

    I made the changes on 2024-05-29 22:28:18

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,3 +2,16 @@
 """
 User model
 """
+from sqlalchemy import (
+    Integer, String, Nullable, Column)
+
+class User:
+    """
+    """
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String(200), nullable=False)
+    hashed_password = Column(String(200), nullable=False)
+    session_id = Column(String(200), nullable=True)
+    reset_token = Column(String(200), nullable=True)
+    
