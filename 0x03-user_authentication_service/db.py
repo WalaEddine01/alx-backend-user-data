58d0b759a23dc5b3a757d7ef39852c1d9d7f3a46
walaeddine01 <walaaeddine33@gmail.com>
2024-05-30 16:24:13 +0200

    I made the changes on 2024-05-30 16:24:13

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -38,3 +38,19 @@ class DB:
         session.add(user)
         session.commit()
         return user
+    
+    def find_user_by(self, *args, **kwargs) -> User:
+        """
+        This method takes in arbitrary keyword arguments and returns the first row
+        found in the users table as filtered by the method's input arguments
+        """
+        for key, value in kwargs.items():
+            if key in User.__dict__:
+                session = self._session
+                if session.get(key, value):
+                    return session.get(key, value)
+                else:
+                    print("Not found")
+            else:
+                print('Invalid')
+
