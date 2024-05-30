673069c40eafd11f422f0fc16d7144d7159d0965
walaeddine01 <walaaeddine33@gmail.com>
2024-05-30 13:24:08 +0200

    I made the changes on 2024-05-30 13:24:08

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,6 +2,7 @@
 """
 User model
 """
+from uuid import uuid4
 from sqlalchemy import (
     Integer, String, Column
     )
@@ -20,3 +21,14 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
+
+    def __init__(self, *args, **kwargs):
+        """
+        """
+        self.id = uuid4()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+        if kwargs.get("id", None) is None:
+                self.id = str(uuid4())
