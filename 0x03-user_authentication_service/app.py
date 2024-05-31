ca7b69787c1adad6d26ace36a1fa8574cfe330c2
walaeddine01 <walaaeddine33@gmail.com>
2024-05-31 12:50:56 +0200

    I made the changes on 2024-05-31 12:50:56

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This is the asic Flask app Module
+"""
+from flask import jsonify, Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def greet():
+    """
+    THis basic greeting method
+    """
+    return jsonify({"message": "Bienvenue"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
