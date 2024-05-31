7706981b1cd9ffca019f85493f563d94007b1856
walaeddine01 <walaaeddine33@gmail.com>
2024-05-31 13:39:05 +0200

    I made the changes on 2024-05-31 13:39:05

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,8 +2,13 @@
 """
 This is the asic Flask app Module
 """
-from flask import jsonify, Flask
+from flask import (
+    jsonify, Flask, request,
+    abort)
+from auth import Auth
 
+
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -14,6 +19,26 @@ def greet():
     """
     return jsonify({"message": "Bienvenue"})
 
+
+@app.route("/users")
+def users():
+    """
+    """
+    data = request.get_json()
+    if data:
+        if 'email' not in request.get_json():
+            abort(400, description="Missing email")
+        if 'password' not in request.get_json():
+            abort(400, description="Missing password")
+    else:
+        abort(400, description="Not a JSON")
+    try:
+        user = AUTH.register_user(data["email"], data["password"])
+    except ValueError:
+        return jsonify({"message": "email already registered"})
+    return jsonify({"email": "{}".format(data["email"]),
+                    "message": "{}".format(user)})
+        
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
