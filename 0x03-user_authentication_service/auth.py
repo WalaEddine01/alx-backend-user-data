4ed349e89863c4de89afb40d31d94ecb8684878b
walaeddine01 <walaaeddine33@gmail.com>
2024-05-31 12:40:27 +0200

    I made the changes on 2024-05-31 12:40:27

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,7 +2,10 @@
 """
 This mdule for auth
 """
+from user import User
 import bcrypt
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -10,3 +13,23 @@ def _hash_password(password: str) -> bytes:
     takes in a password string arguments and returns bytes.
     """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        """
+        try:
+            if self._db.find_user_by(email=email):
+                raise ValueError("User <user's email> already exists")
+        except NoResultFound:
+            pass
+        hashed_password = _hash_password(password=password)
+        user = self._db.add_user(email=email, hashed_password=hashed_password)
+        return user
