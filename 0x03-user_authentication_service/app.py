0aeb0a9b8305d4a3fa234663c53c24e73825080f
walaeddine01 <walaaeddine33@gmail.com>
2024-06-01 17:07:27 +0200

    I made the changes on 2024-06-01 17:07:27

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -89,7 +89,7 @@ def profile():
     session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
     if user:
-        return jsonify({"email": f'{user.email}'}, )
+        return jsonify({"email": f'{user.email}'}, 200)
     abort(403)
 
 
